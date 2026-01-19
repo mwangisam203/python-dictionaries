0b2dd4c7adf43a8eeac29859fc3af06567bb7810
mwangisam203 <mwangisam203@gmail.com>
2026-01-19 02:27:13 -0500

    fast testing

---
+++ main.py
@@ -0,0 +1,9 @@
+mydict = {
+
+  "brand" : "Mercedes",
+  "model" : "GLE 63S",
+  "seats" : 5,
+  "year"  : 2024
+
+}
+print(mydict) #Output: {'brand': 'Mercedes', 'model': 'GLE 63S', 'seats': 5, 'year': 2024}
