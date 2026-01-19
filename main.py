8aa235be031b96897dd4b65b2baa4ff1e53e647f
mwangisam203 <mwangisam203@gmail.com>
2026-01-19 02:44:40 -0500

    more

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 mydict = {
 
   "brand" : "Mercedes",
+  "model" : "S550",
   "model" : "GLE 63S",
   "seats" : 5,
   "year"  : 2024
 
 }
 print(mydict) #Output: {'brand': 'Mercedes', 'model': 'GLE 63S', 'seats': 5, 'year': 2024}
+
+print(mydict["year"]) #Output 2024
+
+                            #NB duplicates are not allowed or are rather ignored
+                            # use the len() function to get the length
+print(len(mydict)) #Output 4
